4bcd110283071a884de1db32af65b806ed28c4e6
Sunil pargi <sunilpargi121@gmail.com>
2020-11-27 11:06:09 +0530

    added player score

---
+++ Jack The Giant 2D/Assets/_Scripts/GameCOntroller/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public void PlayGame()
+	{
+		
+		SceneManager.LoadScene("Gameplay");
+	}
+
+	public void HighScoreMenu()
+	{
+		SceneManager.LoadScene("Highscore");
+	}
+
+	public void OptionsMenu()
+	{
+		SceneManager.LoadScene("OptionMenu");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
